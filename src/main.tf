3ca49129f869a936e979135e381c11d10516054b
jailton.junior <jailton.junior@iterative.com.br>
2021-09-15 17:20:32 -0300

    destruindo a infra

--- src/main.tf
+++ src/main.tf
@@ -3,6 +3,10 @@ terraform {
     digitalocean = {
       source = "digitalocean/digitalocean"
     }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.58.0"
+    }
   }
 
   backend "s3" {
